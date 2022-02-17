be3e0e7f5b0ce2d47c834591fb02decc68a2527a
Laura Gauto <laugauto@hotmail.com>
2022-02-16 23:47:33 -0300

    Cambios en js y cdn de axios

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,39 @@
+let datos = [];
 const obtenerPokemones = async() => {
     //Petición
     try {
-        const respuesta = await axios.get('https://pokeapi.co/api/v2/pokemon/')
-        console.log(respuesta);
+        const respuesta = await axios.get('https://pokeapi.co/api/v2/pokemon?limit=100&offset=200');
+        if (respuesta.status == 200){
+            respuesta.data.results.map(dato => {
+                    axios.get(dato.url).then(respuestaPokemon => {
+                        if (respuestaPokemon.status == 200){
+                            let pokedata = {
+                                id = respuestaPokemon.data.id,
+                                name = dato.name,
+                                image_front_url = respuestaPokemon.data.sprites.front_default,
+                                image_back_url = respuestaPokemon.data.sprites.back_default, 
+                                abilities = respuestaPokemon.data.abilities.map(dataAbility => dataAbility.ability.name),
+                                type = respuestaPokemon.data.types.map(dataType => dataType.type.name),
+                                experience = respuestaPokemon.data.base_experience
+                            }
+                            datos.push(pokedata);
+                        }
+                        console.log(datos, "datos");
+                       /*  return datos; */
+               
+               
+                    })
+                    .catch(error => console.log('ocurrio un error', error))   
+            })    
+        }
     } catch(error) {
         console.log(error);
     }
 }
+
+const escribirPokemones = () => {
+    obtenerPokemones();
+    console.log(datos);
+} 
+
+escribirPokemones();
