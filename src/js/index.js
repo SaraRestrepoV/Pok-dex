caff598af1ed7420c45ed32c740bc4165908f387
Laura Gauto <laugauto@hotmail.com>
2022-02-17 23:29:10 -0300

    Agregar template a escribirPokemon js

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,36 @@
 let datos = [];
+let fondos = {
+    0: '/img/fondo-agua.jpg',
+    1: '/img/fondo-fuego.jpg',
+    2: '/img/fondo-planta.jpg'
+}
 const escribirPokemones = (datos) => {
+    let template = '';
+    let main_pokemon;
     datos.map((dato, i) => {
-        console.log("iteracion", i, dato);
+        let numero_aleatorio = Math.floor(Math.random() * 3);
+        let imagen_aleatoria = fondos[numero_aleatorio];
+        main_pokemon = document.getElementById('main_pokemon');
+        template += `<article class="tarjeta">
+        <img src=${imagen_aleatoria} alt="fondo-tarjeta" class="tarjeta-fondo">
+        <header class="tarjeta-header">
+            <img src=${dato.image_front_url} alt="pokemon" class="tarjeta-header-imagen">
+            <h1 class="tarjeta-titulo">${dato.name} #<span>${dato.id}</span></h1>
+            <p class="tarjeta-tipo-pokemon">${dato.type[0]}</p>
+        </header>
+        <footer class="tarjeta-footer">
+            <div class="tarjeta-footer-habilidad">
+                <h3>Habilidad</h3>
+                <p>${dato.abilities[0]}</p>
+            </div>
+            <div class="tarjeta-footer-experiencia">
+                <h3>N Experiencia</h3>
+                <p>${dato.experience}</p>
+            </div>
+        </footer>
+    </article>`;
     })
+    main_pokemon.innerHTML = template;
 } 
 
 const obtenerPokemones = async() => {
